0883218a0d74645de77c9fc5b62102dfc78181e0
frankNowinski <nowinski.frank@gmail.com>
2015-12-12 13:58:15 -0500

    add valid_move?

--- lib/stock-gains/cli.rb
+++ lib/stock-gains/cli.rb
@@ -8,7 +8,33 @@ class StockGains::CLI
   end
 
   def start
-
+    input = ""
+    unless input == "exit"
+      begin 
+        puts "\nTo view more stock information, enter the number(s)"
+        puts "associated with the stock or enter 'all' to view"
+        puts "all of the stocks in your portfolio."
+        puts "(Separate digits with a space)\n\n"
+        input = gets.strip.scan(/\w+/)
+      end until valid_input?(input)
+    end
   end
 
+  def valid_input?(input)
+    "all".include?(input.first) || input.map(&:to_i).all?{ |n| n.between?(1, StockGains::Stock.all.count)}
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
