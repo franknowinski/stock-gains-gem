a47dd0c99c7449408cfd265a62e7a6e6c21acc4b
frankNowinski <nowinski.frank@gmail.com>
2015-12-13 01:35:14 -0500

    add stock spec

--- spec/01_cli_spec.rb
+++ spec/01_cli_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe 'StockGains::CLI' do 
   klass = StockGains::CLI.new
-  apple = StockGains::Stock.new("Apple Inc.", "113.07", "116.17", "114.94", "148.64", "112.85 - 115.39", "92.00 - 134.54", "12.28", "9.22", " 30")
 
   describe '.instance' do 
     it 'creates an instance' do 
@@ -17,6 +16,11 @@ describe 'StockGains::CLI' do
 
     it 'is true when the input values are valid' do
       expect(klass.valid_input?(["1, 2, 3"])).to be(true)
+    end
+
+    it 'is false when the input is invalid' do 
+      expect(klass.valid_input?(["0"])).to be(false)
+      expect(klass.valid_input?(["none"])).to be(false)
     end
   end
 end
---
+++ spec/02_stock_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'StockGains::Stock' do 
+  apple = StockGains::Stock.new("Apple Inc.", "113.07", "116.17", "114.94", "148.64", "112.85 - 115.39", "92.00 - 134.54", "12.28", "9.22", "30")
+
+  describe '#initialize' do 
+    it 'correctly formats a new stock instance' do 
+      expect(apple.name).to eq("Apple Inc.")
+      expect(apple.cur_price).to eq("113.07")
+      expect(apple.prev_close).to eq("116.17")
+      expect(apple.open).to eq("114.94")
+      expect(apple.year_trgt).to eq("148.64")
+      expect(apple.d_range).to eq("112.85 - 115.39")
+      expect(apple.y_range).to eq("92.00 - 134.54")
+      expect(apple.pe).to eq("12.28")
+      expect(apple.eps).to eq("9.22")
+      expect(apple.shares).to eq("30")
+    end
+  end
+
+  describe '#calculate_days_value' do 
+    it '@days_value is correctly calculated' do 
+      expect(apple.days_value).to eq(-93.0) 
+    end
+  end
+
+  describe '.retrieve_stock' do 
+    it 'retrieves stock values from the Yahoo! Finance API' do 
+      expect(StockGains::Stock.retrieve_stock(["AAPL"])).to eq([["Apple Inc.", "113.07", "116.17", "114.94", "148.64", "112.85 - 115.39", "92.00 - 134.54", "12.28", "9.22"]])
+    end
+  end
+end
