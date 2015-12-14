0527f0e420cd6bac02910c2e07b36ae153cafc94
frankNowinski <nowinski.frank@gmail.com>
2015-12-14 16:51:41 -0500

    configured files

--- lib/stock_gains.rb
+++ lib/stock_gains.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'open-uri'
 require 'csv'
 
-Dir[File.join(File.dirname(__FILE__), "stock-gains", "*.rb")].each {|f| require_relative f}
+Dir[File.join(File.dirname(__FILE__), "stock-gains", "*.rb")].each {|f| require f}
 
 module StockGains
 end
